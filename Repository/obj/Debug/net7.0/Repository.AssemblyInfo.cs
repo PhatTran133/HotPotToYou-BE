f5b1c14ed16940303eec98b1b0fbaf628536f8d8
NamPhan <nampnhse173502@fpt.edu.vn>
2024-06-11 14:27:59 +0700

    CRUD hotpottype

--- Repository/obj/Debug/net7.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net7.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59937caeee922bb1beb7418a36e9804196ea84bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
